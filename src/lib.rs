3291518a5bb7311ff84b16deabf8859a58ed95cf
zhuwenyan02 <zhuwenyan02@meituan.com>
2022-01-12 17:39:55 +0800

    add

---
+++ src/lib.rs
@@ -0,0 +1,94 @@
+use std::fs;
+use std::error::Error;
+use std::env;
+
+pub struct Config {
+    pub query: String,
+    pub filename: String,
+    pub case_sensitive: bool,
+}
+
+impl Config {
+    // 异常类型的返回值是一个全局的字符串字面值
+    pub fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+        let query = (&args[1]).clone();
+        let filename = (&args[2]).clone();
+        // 如果环境变量有CASE_INSENSITIVE，is_err()返回false，即不区分大小写；
+        // 如果环境变量没有CASE_INSENSITIVE，is_err()返回true，即区分大小写
+        let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
+
+        Ok(Config{query, filename, case_sensitive})
+
+    }
+}
+
+// 返回值是一个实现了Error trait的对象，dyn表示是动态的，根据不同的错误类型可以绑定不同的Error trait
+pub fn run(config: &Config) -> Result<(), Box<dyn Error>> {
+    // 通过？表达式来获取成功时的值，如果错误的话会直接返回Error相关值
+    let contents = fs::read_to_string(&config.filename)?;
+    let result= if config.case_sensitive {
+            search(&config.query, &contents)
+        } else {
+            search_case_insensitive(&config.query, &contents)
+        };
+    for re in result {
+        println!("{}", re);
+    }
+    Ok(())
+}
+
+// 函数签名使用了生命周期'a,说明返回值的str和入参contents的生命周期相同，
+// 因为返回的切片应该是入参contents的切片
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut result = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query) {
+            result.push(line);
+        }
+    }
+    result
+}
+// 区分大小写的search匹配
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut result = Vec::new();
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            result.push(line);
+        }
+    }
+    result
+}
+
+#[cfg(test)]
+mod test {
+
+    use super::*;
+
+    #[test]
+    fn on_result() {
+        let query = "duct";
+        let contents = "\
+Rust:\n
+safe, fast, productive.\n
+Pick three\n\
+Duct page.";
+        assert_eq!(vec!["safe, fast, productive."],
+        search(query, contents));
+    }
+
+    #[test]
+    fn case_sensitive() {
+        let query = "rUst";
+        let contents = "\
+Rust:\n
+safe, fast, productive.\n
+Pick three.\n\
+Trust me.";
+        assert_eq!(vec!["Rust:", "Trust me."],
+        search_case_insensitive(query, contents));
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,28 @@
+extern crate minigrep;
+
+use std::env;
+use std::process;
+use minigrep::Config;
+
+// 使用方式：cargo run test poem.txt
+// 设置环境变量CASE_INSENSITIVE
+// export CASE_INSENSITIVE=1，通过echo $CASE_INSENSITIVE进行查看
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    // 使用闭包，闭包参数放在||中，可以在后续进行调用
+    let config = Config::new(&args).unwrap_or_else(|err|{
+        // eprintln!宏向标准错误打印信息
+        eprintln!("Problem parsing arguments {}", err);
+        process::exit(1);
+    });
+    // 测试环境调试的输出
+    println!("query : {}", config.query);
+    println!("filename: {}", config.filename);
+    // 因为run成功时无返回值，因此使用if let仅对错误时进行处理
+    if let Err(e) = minigrep::run(&config) {
+        eprintln!("Application error, {}", e);
+        process::exit(1);
+    }
+
+}
